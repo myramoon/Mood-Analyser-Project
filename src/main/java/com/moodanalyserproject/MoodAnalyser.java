19c9a9a9305b7ae6a54d862f99560b5a58b3c9e6
anamfazal <anamfazal94@gmail.com>
2020-10-22 19:45:17 +0530

    Refactored: functionality code and test cases to take mood message in constructor

--- src/main/java/com/moodanalyserproject/MoodAnalyser.java
+++ src/main/java/com/moodanalyserproject/MoodAnalyser.java
@@ -1,12 +1,18 @@
-/* Purpose: To determine mood using analyseMood method */
+/* Purpose: To refactor the code to take mood message in constructor */
 package com.moodanalyserproject;
 
 public class MoodAnalyser {
+    String message;
 
-    public String analyseMood(String message) {
+    public MoodAnalyser (String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
         if (message.contains("sad"))
             return "SAD";
         else
             return "HAPPY";
     }
 }
+
